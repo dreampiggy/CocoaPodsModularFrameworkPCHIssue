44bd1cad1e679ed2e480902d54ac4254723075cf
DreamPiggy <lizhuoli1126@126.com>
2019-01-28 15:02:40 +0800

    Initial Commit

---
+++ TestModularFrameworkPCHIssue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestModularFrameworkPCHIssue
+//
+//  Created by lizhuoli on 2019/1/28.
+//  Copyright © 2019 SDWebImage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestModularFrameworkPCHIssue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestModularFrameworkPCHIssue
+//
+//  Created by lizhuoli on 2019/1/28.
+//  Copyright © 2019 SDWebImage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
